aaaf4097b16d95d928590a24c3286a66c74b2dd0
Qayzan <50222777+Qayzan@users.noreply.github.com>
2026-03-30 17:28:07 +0800

    Pin dependency versions, add --noconfirm to build

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ py -m pip install pyinstaller
 
 echo.
 echo Building rbs-checker.exe...
-py -m PyInstaller rbs-checker.spec --clean
+py -m PyInstaller rbs-checker.spec --clean --noconfirm
 
 echo.
 echo Done! Distributable is in: dist\rbs-checker\
